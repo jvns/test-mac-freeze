1cdd1e7ce216b0a9234331ba36f732d11bdbd6e6
Julia Evans <julia@jvns.ca>
2018-01-28 00:25:21 -0500

    make it work with true

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use mach::port::{mach_port_name_t, MACH_PORT_NULL};
 use mach::traps::mach_task_self;
 
 pub fn main() {
-    let mut command = Command::new("/usr/bin/ruby").spawn().unwrap();
+    let mut command = Command::new("/usr/bin/true").spawn().unwrap();
     let pid = command.id() as pid_t;
     let task_id = task_for_pid(pid).expect("Failed to get task ID");
     command.kill().unwrap();
@@ -18,13 +18,11 @@ pub fn main() {
 pub fn task_for_pid(pid: pid_t) -> io::Result<mach_port_name_t> {
     let mut task: mach_port_name_t = MACH_PORT_NULL;
     unsafe {
-        println!("Running task_for_pid...");
         let result = mach::traps::task_for_pid(mach_task_self(), pid as c_int, &mut task);
         println!("Done running task_for_pid...");
         if result != KERN_SUCCESS {
             return Err(io::Error::last_os_error());
         }
     }
-
     Ok(task)
 }
